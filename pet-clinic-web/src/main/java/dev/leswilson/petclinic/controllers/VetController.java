effbbbe21cad2849103527e5bb8f0fb81df9ebe5
leswilson <les.wilson@capgemini.com>
2019-08-01 17:54:57 +0100

    adding Vet index.html and controller. closes #7

---
+++ pet-clinic-web/src/main/java/dev/leswilson/petclinic/controllers/VetController.java
@@ -0,0 +1,12 @@
+package dev.leswilson.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VetController {
+    @GetMapping(value={"/vets", "/vets/index", "/vets/index.html"})
+    public String index() {
+        return "vets/index";
+    }
+}
