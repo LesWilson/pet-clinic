30830163ba6e797c63422086e229aa172e2d4e69
leswilson <les.wilson@capgemini.com>
2019-08-02 19:41:52 +0100

    Displaying list of vets.  closes #15

--- pet-clinic-web/src/main/java/dev/leswilson/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/dev/leswilson/petclinic/controllers/VetController.java
@@ -1,12 +1,22 @@
 package dev.leswilson.petclinic.controllers;
 
+import dev.leswilson.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VetController {
+
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping(value={"/vets", "/vets/index", "/vets/index.html"})
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
