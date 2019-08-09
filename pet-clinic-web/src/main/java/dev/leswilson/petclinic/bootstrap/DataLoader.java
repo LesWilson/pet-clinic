d0c9abf14b6a1467da77bf3faff80461d23f9a8d
leswilson <les.wilson@capgemini.com>
2019-08-09 10:52:29 +0100

    updating dataloader to add another pet to owner2

--- pet-clinic-web/src/main/java/dev/leswilson/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/dev/leswilson/petclinic/bootstrap/DataLoader.java
@@ -45,11 +45,13 @@ public class DataLoader implements CommandLineRunner {
         owner1.setLastName("Balboa");
         addAddress(owner1, "123 Low Street", "", "Buckhurst Hill", "Essex", "UK", "IG10 6GG");
         owner1.setTelephone("0208 765 4321");
+
         Pet pet1 = new Pet();
         pet1.setBirthDate(LocalDate.of(2015, 11, 12));
         pet1.setPetType(dog);
         pet1.setName("Micky");
         pet1.setOwner(owner1);
+
         owner1.getPets().add(pet1);
         ownerService.save(owner1);
 
@@ -58,11 +60,20 @@ public class DataLoader implements CommandLineRunner {
         owner2.setLastName("Creed");
         addAddress(owner2, "123 High Street", "", "Woodford", "Greater London", "UK", "IG9 6AA");
         owner2.setTelephone("0208 123 4567");
+
         Pet pet2 = new Pet();
         pet2.setBirthDate(LocalDate.of(2017, 1, 23));
         pet2.setPetType(cat);
         pet2.setName("Ricky");
         pet2.setOwner(owner2);
+
+        Pet pet3 = new Pet();
+        pet3.setBirthDate(LocalDate.of(2015, 11, 12));
+        pet3.setPetType(horse);
+        pet3.setName("Nellie");
+        pet3.setOwner(owner1);
+
+        owner2.getPets().add(pet3);
         owner2.getPets().add(pet2);
         ownerService.save(owner2);
 
